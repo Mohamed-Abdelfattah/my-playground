2884269cf35b74c924a5f91d29d0131cb9b1e4e9
Mohamed Gamal <magamal9292@gmail.com>
2022-01-25 04:05:40 +0200

    initiated the skelton

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import TrelloPage from './Trello-Homemade/Trello-Page';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  //
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>My playgrond</p>
+            <Link to="/trello">Trello HomeMade</Link>
+          </header>
+        </div>
+      </Route>
+      <Route path="/trello">
+        <TrelloPage />
+      </Route>
+    </Switch>
+  );
+};
 
 export default App;
---
+++ src/Trello-Homemade/Trello-Page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const TrelloPage = () => {
+  return (
+    <div>
+      <h1>Trello homemade</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+export default TrelloPage;
